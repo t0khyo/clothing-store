b49a1ec07b00a78003553be538596637d4f064db
t0khyo <abdullrhman.eltoukhy@gmail.com>
2024-08-15 18:50:42 +0300

    added category

--- src/main/java/com/t0khyo/clothing_store/controller/CategoryController.java
+++ src/main/java/com/t0khyo/clothing_store/controller/CategoryController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RequestMapping("/api/v1/category")
 @RestController
 public class CategoryController {
-    private CategoryService categoryService;
+    private final CategoryService categoryService;
 
     @GetMapping("/{id}")
     public ResponseEntity<Category> getById(Long id) {
